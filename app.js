ff639ea9c26decb9eb733ffe251767e6dec778e8
Miracle lawrence <lawrencemiracle72@gmail.com>
2024-12-05 21:52:48 +0100

    done with gmail

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var nodemailer = require('nodemailer');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+const path = require('path');
+
+
+function sendmail(body){
+	(async () => {
+		const { default: hbs } = await import('nodemailer-express-handlebars');
+		//send mail
+		var transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: "miracle.lawrence43@gmail.com",
+				pass: 'dklq htkb kcce drha',
+			},
+		});
+		
+		
+		// Step 2: Configure Handlebars as the template engine
+		transporter.use('compile', hbs({
+				viewEngine: {
+					extName: '.hbs',
+					defaultLayout: false, // No default layout since we're not using partials
+				},
+				viewPath: path.join(__dirname, '../views'), // Path to the templates
+				extName: '.hbs', // Template file extension
+			})
+		);
+
+		const mail = {
+		   from: 'offer@miraclelawrence',
+		   to: body.email,
+		   subject: 'Get A Professional Website for only $250!',
+		   template: 'email1',
+		   context: {
+			   name: body.name,
+			   company: body.company
+		   }
+		}
+		
+		try {
+			const info = await transporter.sendMail(mail);
+			console.log('Email sent: ', info.response);
+		} catch (error) {
+			console.error('Error sending email: ', error);
+		}
+	})();
+}
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +59,9 @@ router.get('/', function(req, res, next) {
 
 /* send proposal */
 router.post('/send-prop', function(req, res, next) {
-  console.log("fd")
+  var details = req.body
+  sendmail(details);  
+  res.render('index', { title: 'Sent' });
 });
 
 module.exports = router;
